6a39f597614d08d43aa56ccf82551d501ea10b9e
Johann Behrens <me@jbehrens.nu>
2025-04-29 12:32:55 +0200

    test: add test coverage for existing cases

--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -4,4 +4,134 @@ import Testing
 import GildedRose
 
 @Suite("GildedRose")
-final class GildedRoseTests {}
+struct GildedRoseTests {
+    @Test
+    func test_dexterityVest_whenSellInIsPositive_shouldDecreaseQualityByOne() {
+        let items = [Item(name: "Dexterity Vest", sellIn: 10, quality: 20)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 9)
+        #expect(items[0].quality == 19)
+    }
+
+    @Test
+    func test_dexterityVest_whenSellInZeroDays_shouldDecreaseQualityByTwo() {
+        let items = [Item(name: "Dexterity Vest", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == -1)
+        #expect(items[0].quality == 8)
+    }
+
+    @Test
+    func test_agedBrie_whenSellInIsPositive_shouldIncreaseQualityByOne() {
+        let items = [Item(name: "Aged Brie", sellIn: 2, quality: 0)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 1)
+        #expect(items[0].quality == 1)
+    }
+
+    @Test
+    func test_agedBrie_whenSellInDecreasesTwoTimes_shouldIncreaseQualityByTwo() {
+        let items = [Item(name: "Aged Brie", sellIn: 2, quality: 0)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 0)
+        #expect(items[0].quality == 2)
+    }
+
+    @Test
+    func test_agedBrie_whenQualityIsFifty_andADayPasses_itIsNeverAboveQualityOfFifty() {
+        let items = [Item(name: "Aged Brie", sellIn: 1, quality: 50)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].quality == 50)
+    }
+
+    @Test
+    func test_elixirOfMongoose_whenSellInIsPositive_shouldDecreaseQualityByOne() {
+        let items = [Item(name: "Elixir of Mongoose", sellIn: 5, quality: 7)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 4)
+        #expect(items[0].quality == 6)
+    }
+
+    @Test
+    func test_elixirOfMongoose_whenSellInIsZero_shouldDecreaseQualityByTwo() {
+        let items = [Item(name: "Elixir of Mongoose", sellIn: 0, quality: 7)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == -1)
+        #expect(items[0].quality == 5)
+    }
+
+    @Test
+    func test_sulfuras_whenCycleHappens_shouldNotDecreaseOrIncrease() {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 0)
+        #expect(items[0].quality == 80)
+    }
+
+    @Test
+    func test_sulfuras_whenCycleHappensWithNegativeSellIn_shouldNotDecreaseOrIncrease() {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == -1)
+        #expect(items[0].quality == 80)
+    }
+
+    @Test
+    func test_backstagePasses_whenSellInIsMoreThanTen_qualityShouldIncreaseByOne() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 20, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 19)
+        #expect(items[0].quality == 11)
+    }
+
+    @Test
+    func test_backstagePasses_whenSellInIsLessThanTen_shouldIncreaseQualityByOne() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 9)
+        #expect(items[0].quality == 12)
+    }
+
+    @Test
+    func test_backstagePasses_whenSellInIsLessThanFive_shouldIncreaseQualityByOne() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == 4)
+        #expect(items[0].quality == 13)
+    }
+
+    @Test
+    func test_backstagePasses_whenSellInIsZero_shouldSetQualityToZero() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        #expect(items[0].sellIn == -1)
+        #expect(items[0].quality == 0)
+    }
+}
