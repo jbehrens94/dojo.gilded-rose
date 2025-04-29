499beee8de67451708256d2a8118fc879f99338a
Johann Behrens <me@jbehrens.nu>
2025-04-29 12:50:52 +0200

    chore: clean-up superfluous code paths for Sulfuras

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -14,9 +14,7 @@ public class GildedRose {
 
             if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                 if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
+                    items[i].quality = items[i].quality - 1
                 }
             } else {
                 if items[i].quality < 50 {
@@ -44,9 +42,7 @@ public class GildedRose {
                 if items[i].name != "Aged Brie" {
                     if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                         if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
+                            items[i].quality = items[i].quality - 1
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality
